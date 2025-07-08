9f6376b516fe9684796025a2d57b4d8822211449
jeremydburgess <215720057+jeremydburgess@users.noreply.github.com>
2025-07-08 10:38:38 -0400

    Remove MANE_Plus_Clinical as a default flag column

--- R/annotateGTF.R
+++ R/annotateGTF.R
@@ -23,7 +23,7 @@ annotateGTF <- function(
       "GENCODE_Primary",
       "Ensembl_canonical",
       "MANE_Select",
-      "MANE_Plus_Clinical"
+#      "MANE_Plus_Clinical" # Probably not really a useful flag to include - it is very limited and doesn't include MANE_Select
     ),
     metadata_urls = list(
       entrez = "https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_48/gencode.v48.metadata.EntrezGene.gz",
